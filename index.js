3dbab234b42d99d98342cc5df511312073a80d87
Amelia Rahman <amel.rahman5@gmail.com>
2023-06-20 17:51:06 +0700

    change response data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const user = require('./user');
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -13,30 +13,40 @@ app.get('/', (req, res) => {
 app.get('/api/v1/users', (req, res) => {
     console.log(req.query, 'THIS IS FROM REQUEST QUERY???')
     const result = user.getAllUser(req.query)
-    res.send(result);
+    res.send({
+        data: result.data
+    });
 })
 
 // METHOD GET untuk certain id
 app.get('/api/v1/users/:id', (req, res) => {
     const result = user.getUserById(req.params.id)
-    res.send(result)
+    res.send({
+        data: result
+    })
 })
 
 
 // METHOD untuk insert data
 app.post('/api/v1/users', (req, res) => {
     const result = user.insertNewData(req.body)
-    res.send(result);
+    res.send({
+        data: result
+    });
 })
 
 app.put('/api/v1/users/:id', (req, res) => {
     const result = user.updateData(req.params.id, req.body)
-    res.send(result);
+    res.send({
+        data: result
+    });
 })
 
 app.delete('/api/v1/users/:id', (req, res) => {
     const result = user.deleteData(req.params.id)
-    res.send(result);
+    res.send({
+        data: result
+    });
 })
 
 // app.put('/', (req, res) => {
