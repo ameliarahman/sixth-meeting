fba511b54d13721a9122f1a028efd83f1dc75a7c
Jenifla <jeniflanurhidayah@gmail.com>
2023-06-29 21:19:18 +0700

    final project web

--- index.js
+++ index.js
@@ -1,58 +1,128 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const user = require('./user');
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const mahasiswa = require('./mahasiswa');
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route untuk halaman utama
 app.get('/', (req, res) => {
-    res.send("Hello World from API from GET")
-})
+  res.render('index');
+});
 
-// METHOD GET
-app.get('/api/v1/users', (req, res) => {
-    console.log(req.query, 'THIS IS FROM REQUEST QUERY???')
-    const result = user.getAllUser(req.query)
-    res.send({
-        data: result.data
-    });
-})
+// Route untuk menampilkan halaman profile
+app.get('/profile', (req, res) => {
+  const dataMahasiswa = [{
+    name: "Jeni Fla Nurhidayah",
+    nim: "V3922024",
+    address: "Madiun"
+    }]
+  res.render('profile', { mahasiswa: dataMahasiswa });
+});
 
-// METHOD GET untuk certain id
-app.get('/api/v1/users/:id', (req, res) => {
-    const result = user.getUserById(req.params.id)
-    res.send({
-        data: result
-    })
-})
+// Route untuk menampilkan halaman list mahasiswa
+app.get('/mahasiswa', (req, res) => {
+  const allMahasiswa = mahasiswa.getAllMahasiswa();
+  res.render('list-mahasiswa', { mahasiswa: allMahasiswa });
+});
 
+// Route untuk hapus data mahasiswa
+app.delete('/mahasiswa/delete/:id', (req, res) => {
+    const id = req.params.id;
+    mahasiswa.deleteMahasiswa(id);
+    res.redirect('/mahasiswa');
+});
 
-// METHOD untuk insert data
-app.post('/api/v1/users', (req, res) => {
-    const result = user.insertNewData(req.body)
-    res.send({
-        data: result
-    });
-})
+// Route untuk update data mahasiswa
+app.put('/mahasiswa/update/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedMahasiswa = {
+    id: id,
+    name: req.body.name,
+    nim: req.body.nim,
+    address: req.body.address
+  };
+  mahasiswa.updateMahasiswa(updatedMahasiswa);
+  res.redirect('/mahasiswa');
+});
 
-app.put('/api/v1/users/:id', (req, res) => {
-    const result = user.updateData(req.params.id, req.body)
-    res.send({
-        data: result
-    });
-})
-
-app.delete('/api/v1/users/:id', (req, res) => {
-    const result = user.deleteData(req.params.id)
-    res.send({
-        data: result
-    });
-})
-
-// app.put('/', (req, res) => {
-//     res.send("Hello World from API from PUT")
-// })
+// Route untuk tambah data mahasiswa
+app.post('/mahasiswa/insert', (req, res) => {
+  const newMahasiswa = {
+    id: req.body.id,
+    name: req.body.name,
+    nim: req.body.nim,
+    address: req.body.address
+  };
+  mahasiswa.insertMahasiswa(newMahasiswa);
+  res.redirect('/mahasiswa');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
+
+
+// const express = require('express')
+// const app = express()
+// const port = 3001
+// const user = require('./user');
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.json())
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World from API from GET")
+// })
+
+// // METHOD GET
+// app.get('/api/v1/users', (req, res) => {
+//     console.log(req.query, 'THIS IS FROM REQUEST QUERY???')
+//     const result = user.getAllUser(req.query)
+//     res.send({
+//         data: result.data
+//     });
+// })
+
+// // METHOD GET untuk certain id
+// app.get('/api/v1/users/:id', (req, res) => {
+//     const result = user.getUserById(req.params.id)
+//     res.send({
+//         data: result
+//     })
+// })
+
+
+// // METHOD untuk insert data
+// app.post('/api/v1/users', (req, res) => {
+//     const result = user.insertNewData(req.body)
+//     res.send({
+//         data: result
+//     });
+// })
+
+// app.put('/api/v1/users/:id', (req, res) => {
+//     const result = user.updateData(req.params.id, req.body)
+//     res.send({
+//         data: result
+//     });
+// })
+
+// app.delete('/api/v1/users/:id', (req, res) => {
+//     const result = user.deleteData(req.params.id)
+//     res.send({
+//         data: result
+//     });
+// })
+
+// // app.put('/', (req, res) => {
+// //     res.send("Hello World from API from PUT")
+// // })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
