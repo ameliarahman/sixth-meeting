d3c12247ce96bbd24ee87da2a46e644ac91473b4
arielfajar <ariel.fajarh@gmail.com>
2023-06-30 23:24:33 +0700

    sixth meeting ariel

---
+++ database.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql2');
+
+// Konfigurasi koneksi ke database
+const databaseconfig = {
+  host: 'localhost',
+  user: 'root',
+  database: 'lastmeeting'
+};
+
+// Membuat koneksi ke database
+const connection = mysql.createConnection(databaseconfig);
+
+// Membuka koneksi
+connection.connect((error) => {
+  if (error) {
+    console.error('Koneksi database gagal:', error);
+  } else {
+    console.log('Koneksi database berhasil');
+  }
+});
+
+// Fungsi untuk menambahkan data mahasiswa ke database
+function insertMahasiswa(mahasiswa) {
+  const { name, nim, address } = mahasiswa;
+  const query = `INSERT INTO mahasiswa (name, nim, address) VALUES (?, ?, ?)`;
+  connection.query(query, [name, nim, address], (error, results) => {
+    if (error) throw error;
+    console.log('Data mahasiswa berhasil ditambahkan');
+  });
+}
+
+// Fungsi untuk update data mahasiswa berdasarkan ID
+function updateMahasiswa(id, updatedMahasiswa) {
+  const { name, nim, address } = updatedMahasiswa;
+  const query = `UPDATE mahasiswa SET name = ?, nim = ?, address = ? WHERE id = ?`;
+  connection.query(query, [name, nim, address, id], (error, results) => {
+    if (error) throw error;
+    console.log('Data mahasiswa berhasil diperbarui');
+  });
+}
+
+// Fungsi untuk menghapus data mahasiswa dari database berdasarkan ID
+function deleteMahasiswa(id) {
+  const query = `DELETE FROM mahasiswa WHERE id = ?`;
+  connection.query(query, [id], (error, results) => {
+    if (error) throw error;
+    console.log('Data mahasiswa berhasil dihapus');
+  });
+}
+
+module.exports = {
+  insertMahasiswa,
+  updateMahasiswa,
+  deleteMahasiswa
+};
